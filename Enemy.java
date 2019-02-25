0b994e09eaa9784c0536894c8ab68a9393508962
VladMarianIlie <47425930+VladMarianIlie@users.noreply.github.com>
2019-02-25 08:55:18 -0700

    Add files via upload

---
+++ Enemy.java
@@ -0,0 +1,28 @@
+public class Enemy extends ActiveEntity {
+
+  Enemy(int baseDamage, int baseHealth) {
+    healthMax = baseHealth ;
+    health = healthMax ;
+    damage = baseDamage ;
+    name = "Enemy" ;
+    team = "enemy" ;
+  }
+
+  @Override
+  void AttackLogic(int direction) {
+    //Enemy Attacks
+  }
+
+  @Override
+  void HealthThresholdEvents() {
+    if(health <= 0) {
+      //Enemy Dies
+    }
+  }
+
+  @Override
+  void checkInteraction(Entity initiator) {
+    //Action
+  }
+
+}
