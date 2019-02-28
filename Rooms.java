01e65ee02e63752d05c2c02af3ac59fd91d5fa54
erickjcalder <47425842+erickjcalder@users.noreply.github.com>
2019-02-28 12:28:43 -0700

    Add files via upload

--- Rooms.java
+++ Rooms.java
@@ -1,34 +1,80 @@
+/**
+ * This class is used to store the basic properties of all rooms. Currently it stores all possible exits as well as
+ * whether or not the room has an event that need to be resolved. More detailed properties will be added later
+ */
 public class Rooms {
 
-    //VARIABLES
+
+    //INSTANCE VARIABLES
+    /**
+     * Simple boolean array that stores the existence of room exits in the North, East, South and West in that order
+     */
     private boolean[] doorways;
+
+    /**
+     * Simple boolean that can tell the game whether or not the current room has an event that is still active. Possible
+     * events include item pickups, enemy encounters, and any other similar interaction
+     */
     private boolean unresolvedEvent;
 
+
     //CONSTRUCTORS
+
+    /**
+     * If no arguments have been provided, all arguments are set to false automatically, and it is assumed that the
+     * room is empty and inaccessible
+     */
     public Rooms() {
         doorways = new boolean[] {false, false, false, false};
         unresolvedEvent = false;
     }
 
+    /**
+     * Allows creation of rooms with custom variables
+     * @param north determines if an exit exists in direct north
+     * @param east determines if an exit exists in direct east
+     * @param south determines if an exit exists in direct south
+     * @param west determines if an exit exists in direct west
+     * @param event determines if room has an unresolved event
+     */
     public Rooms(boolean north, boolean east, boolean south, boolean west, boolean event) {
         doorways = new boolean[] {north, east, south, west};
         unresolvedEvent = event;
     }
 
+    /**
+     * Copy constructor used to return copies of template rooms that exist in the Map class
+     * @param toCopy the Rooms object to copy
+     */
     public Rooms(Rooms toCopy) {
         doorways = toCopy.getDoorways();
         unresolvedEvent = toCopy.getEvent();
     }
 
+
     //GETTERS
+
+    /**
+     * Checks to verify that the room has an exit in a specific direction
+     * @param index determines which direction to check, with 0 = North, 1 = East, 2 = South, and 3 = West
+     * @return True if exit exists and False if not
+     */
     public boolean checkRoomExit(int index) {
         return doorways[index];
     }
 
+    /**
+     * Checks to see if the room has an event
+     * @return True if event exists, False otherwise
+     */
     public boolean getEvent() {
         return unresolvedEvent;
     }
 
+    /**
+     * Used to retrieve a full array of all exits
+     * @return boolean array of all exits
+     */
     public boolean[] getDoorways() {
         boolean[] doorwaysCopy = new boolean[this.doorways.length];
         for (int i = 0; i < this.doorways.length; i++) {
@@ -37,7 +83,14 @@ public class Rooms {
         return doorwaysCopy;
     }
 
+
     //SETTER
+
+    /**
+     * Used to open new doorways on map. For example, if a locked door exists, and the player unlocks it, this will
+     * change the status of that exit
+     * @param index Determines the direction in which a path is opened
+     */
     public void openNewDoorway(int index) {
         doorways[index] = true;
     }
--- TextGame.java
+++ TextGame.java
@@ -1,42 +1,65 @@
 import java.util.Scanner;
 
+/**
+ * This class exists to handle all events in the text based version of the game. It will not serve any purpose in the
+ * final 2D iteration of the game.
+ */
 public class TextGame {
 
     //INSTANCE VARIABLES
     /**
-     * These handle the general size of map, as well as the room types contained in the map array. In the 2D version of
-     * the game, map creation will be handled differently, but as far as the text game is concerned, a more rigid map is
-     * better, as it gives us tighter control on how the game can play out, which is important as player agency is
-     * limited
+     * gameMap is a map object that contains the 2D array of room values that are in turn used to generate all rooms
+     * necessary for the floor on which the game is played. Length and height are provided on map generation, which
+     * determines the number of rooms along the x and y axis respectively. Map generation is currently static, but
+     * these parameters will eventually be randomized so as to make each game unique
      */
     private static Map gameMap = new Map(3,3);
+
+    /**
+     * row1, row2, and row3 are simple integer arrays that contain reference numbers for different varieties of rooms.
+     * When a game is started, these arrays are sent to the game map object to tell it what rooms it needs for the Rooms
+     * array. Like gameMap, these rooms are currently static, but will be made to be dynamic in the future
+     */
     private static int[] row1 = {2, 9, 7};
     private static int[] row2 = {0, 6, 8};
     private static int[] row3 = {0, 5, 4};
 
 
     /**
-     * General scanner used to handle player input
+     * This scanner object exists to handle general payer input, pressing enter to advance text, submitting commands,
+     * etc. It will likely be replaced by JavaFX event handlers for the 2D version of our game
      */
     private static Scanner utilityScanner = new Scanner(System.in);
 
 
     /**
-     * This is effectively the player class, but simplified for the text game
+     * lastDirection stores the direction from which the room was entered. This would theoretically communicate with
+     * the RPGLayer to enable certain functions like retreat to function properly, however, that was not implemented
+     * in our text game
      */
     private static String lastDirection;
+
+    /**
+     * xCoord and yCoord determine which room the player is currently in. In the text game this means that they
+     * increment whenever the player types a direction to move in, but the 2D game equivalent will increment when the
+     * player touches the edge of the screen at a valid exit point
+     */
     private static int yCoord;
     private static int xCoord;
 
 
     /**
-     * Stores the most recent player input
+     * Stores the input of the player from the console. Again, a band-aid solution for the text game which will be
+     * obsoleted by event handlers in the 2D animated version
      */
     private static String lastInput;
 
 
     //METHODS
 
+    /**
+     * Checks the current room and outputs all possible exits to the console in a single line
+     */
     private static void printPossibleExits() {
         boolean previousEntry = false;
 
@@ -72,6 +95,12 @@ public class TextGame {
         }
     }
 
+    /**
+     * Is called whenever a room transition is needed. Increments the player's x and y coordinates appropriately. Will
+     * tell the player if their action is invalid.
+     * @param direction tells the transition handler which direction the player is attempting to move, and how to adjust
+     *                  the x and y coordinates if the move is valid
+     */
     private static void roomTransition(String direction) {
         switch (direction) {
 
@@ -121,45 +150,59 @@ public class TextGame {
         }
     }
 
+    /**
+     * This method is the first stop for any player input once the game has begun.
+     * @param input Is used to determine what needs to be called next. Each possible input is explained in greater
+     *              detail in the line comments below
+     */
     private static void inputHandler(String input) {
         input = input.toLowerCase();
         switch (input) {
 
+            //Simply tells the player that their input is invalid and nothing has happened
             default: {
                 System.out.println("Invalid input");
                 break;
             }
 
+            //Used to end the game prematurely
             case "exit game": {
                 System.out.println("GAME OVER");
                 System.exit(0);
             }
 
+            //Reprints the help message which tells the player what actions they are able to do at any time
             case "help": {
                 printHelpMessage();
                 break;
             }
 
+            //Redirects to the room transition handler with direction north
             case "move north": {
                 roomTransition("north");
                 break;
             }
 
+            //Redirects to the room transition handler with direction east
             case "move east": {
                 roomTransition("east");
                 break;
             }
 
+            //Redirects to the room transition handler with direction south
             case "move south": {
                 roomTransition("south");
                 break;
             }
 
+            //Redirects to the room transition handler with direction west
             case "move west": {
                 roomTransition("west");
                 break;
             }
 
+            //Also used to end the game prematurely, but in a way that makes sense in regards to the scenario. Intended
+                //to be a joke
             case "kill self": {
                 System.out.println("You take a rock out from your pouch, load your sling, and hold it against your" +
                         " head.\nYou pull back the elastic as far as you can and fire a rock directly at your own" +
@@ -167,6 +210,7 @@ public class TextGame {
                 System.exit(0);
             }
 
+            //Prints the map for the player to see
             case "map": {
                 gameMap.printMap();
                 break;
@@ -174,48 +218,79 @@ public class TextGame {
         }
     }
 
+    /**
+     * Called at the start of the game and whenever the player enters "help". Informs the player of all commands which
+     * they can enter at that time
+     */
     private static void printHelpMessage() {
         System.out.println("- To move to another room, type 'move <direction you would like to move>'\n" +
                 "- To view the map, type 'map'\n" +
                 "- To view this message again, type 'help'\n");
     }
 
+
+
     //MAIN
 
     public static void main(String[] args) {
-        //Build the map
+
+        //All three row arrays are taken and added to the gameMaps roomType array
         gameMap.setRoomTypeByRow(0, row1);
         gameMap.setRoomTypeByRow(1, row2);
         gameMap.setRoomTypeByRow(2, row3);
+
+        //Reading the values provided by the rows, the appropriate room objects are created for each position
         gameMap.buildCurrentFloor();
+
+        //Co-ordinates are set to the default position
         yCoord = 0;
         xCoord = 0;
 
-        //Title screen
+        //A simple title screen is printed out
         System.out.println("________________________________________\n");
         System.out.println("             <working title>            ");
         System.out.println("________________________________________");
+
+        //User is prompted to press enter to begin the game, scanner is used to wait for that input
         System.out.println("Press enter to begin");
         utilityScanner.nextLine();
 
-        //Setup scenario
+        //Basic message outlining player scenario is printed
         System.out.println("You awaken in a dimly lit room with nothing but a slingshot and a small pouch filled with" +
                 " rocks.\nYou see a map on the wall, this seems to be some sort of maze. There appears to be an exit" +
                 " \nsomewhere nearby.\n");
+
+        //Help message is automatically printed to inform the player of possible actions
         printHelpMessage();
+
+        //Solid lines are used to make reading events more easy
         System.out.println("________________________________________");
 
-        //Main loop
+        //Main game play loop is initiated
         do {
+
+            //At the start of each loop, all possible exits are stated for the user to see
             printPossibleExits();
+
+            //User is then prompted for their input
             System.out.print("\nWhat would you like to do?: ");
+
+            //Their input is taken by the scanner and stored in the lastInput variable
             lastInput = utilityScanner.nextLine();
+
+            //Newline printed for ease of reading
             System.out.print("\n");
+
+            //User input is sent to the input handler
             inputHandler(lastInput);
+
+            //Another line for ease of reading
             System.out.println("________________________________________");
+
+        //Game checks for end condition, which is current that the players roomType corresponds to a 4
         } while (gameMap.getRoomType(yCoord, xCoord) != 4);
 
-        //Ending message
+        //Message is printed to inform payer they have achieved the end goal
         if (gameMap.getRoomType(yCoord, xCoord) == 4) {
             System.out.println("Congratulations! You've made it to the end of the maze");
         }
