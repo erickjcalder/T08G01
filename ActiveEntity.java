5cd74dd879800c7baedf849f90f9326a4c454264
jhyan-justin-chen <47425966+jhyan-justin-chen@users.noreply.github.com>
2019-03-07 17:42:02 -0700

    Update to Entity backbone
    Major update to entity backbone to add new values (armor, damagemult), and to increase encapsulation. Adds many new methods and restrictions, both related to and unrelated to encapsulation. This will break code.

--- ActiveEntity.java
+++ ActiveEntity.java
@@ -6,27 +6,221 @@ abstract class ActiveEntity extends Entity
     /**
      * Represents maximum health of the entity.
      */
-    int healthMax;
+    private int healthMax = 100;
 
     /**
-     * Represents current health of the entity.
+     * Represents current health of the entity. Cannot exceed maximum health.
      */
-    int health;
+    private int health = 100;
+
+    /**
+     * Represents armor, or damage reduction of entity.
+     * Cannot be less than 0 or greater than 100.
+     * Armor is an integer that is then converted to a percentage
+     * of damage reduction based on a formula.
+     */
+    private int armor = 0;
+
+    /**
+     * Represents maximum velocity in an axis.
+     */
+    private float maxVelocity = 10;
 
     /**
      * Represents velocity of movement.
      */
-    float velocityX;
-    float velocityY;
+    private float velocityX = 0;
+    private float velocityY = 0;
+
+    /**
+     * Returns maximum health of entity.
+     * @return maximum health of entity.
+     */
+    public int getHealthMax()
+    {
+        return healthMax;
+    }
+
+    /**
+     * Sets maximum health of entity.
+     * @param healthMax maximum health of entity.
+     */
+    protected void setHealthMax(int healthMax)
+    {
+        this.healthMax = healthMax;
+    }
+
+    /**
+     * Gets current health of entity.
+     * @return current health of entity.
+     */
+    public int getHealth()
+    {
+        return health;
+    }
+
+    /**
+     * Sets current health of entity, up to maximum health.
+     * @param health current health of entity.
+     */
+    protected void setHealth(int health)
+    {
+        this.health = health;
+
+        if(this.health > this.healthMax)
+        {
+            this.health = this.healthMax;
+        }
+    }
+
+    /**
+     * Gets armor value of entity.
+     * @return armor value of entity.
+     */
+    public int getArmor()
+    {
+        return armor;
+    }
+
+    /**
+     * Sets armor value of entity, where armor is between 0 and 100.
+     * @param armor armor value of entity.
+     */
+    protected void setArmor(int armor)
+    {
+        this.armor = armor;
+        if(this.armor > 100)
+        {
+            this.armor = 100;
+        }
+        else if(this.armor < 0)
+        {
+            this.armor = 0;
+        }
+    }
+
+    /**
+     * Increases velocity of entity, up to maximum value.
+     * @param velocityX X-value of velocity.
+     * @param velocityY Y-value of velocity.
+     */
+    protected void addVelocity(float velocityX, float velocityY)
+    {
+        this.velocityX += velocityX;
+        this.velocityY += velocityY;
+
+        if(this.velocityX > this.maxVelocity)
+        {
+            this.velocityX = this.maxVelocity;
+        }
+        else if(this.velocityX < this.maxVelocity * -1)
+        {
+            this.velocityX = this.maxVelocity * -1;
+        }
+        else if(this.velocityY > this.maxVelocity)
+        {
+            this.velocityY = this.maxVelocity;
+        }
+        else if(this.velocityY < this.maxVelocity * -1)
+        {
+            this.velocityY = this.maxVelocity * -1;
+        }
+    }
+
+    /**
+     * Gets X-value of velocity.
+     * @return X-value of velocity.
+     */
+    protected float getVelocityX()
+    {
+        return velocityX;
+    }
+
+    /**
+     * Gets Y-value of velocity.
+     * @return Y-value of velocity.
+     */
+    protected float getVelocityY()
+    {
+        return velocityY;
+    }
+
+    protected void normalizeVelocity()
+    {
+        if(this.velocityX > 0)
+        {
+            this.velocityX -= 1;
+        }
+        else if(this.velocityX < 0)
+        {
+            this.velocityX += 1;
+        }
+        else if(this.velocityY > 0)
+        {
+            this.velocityY -= 1;
+        }
+        else if(this.velocityY < 0)
+        {
+            this.velocityY += 1;
+        }
+    }
 
     /**
      * Represents events caused by health.
      */
-    abstract void HealthThresholdEvents();
+    protected abstract void HealthThresholdEvents();
 
     /**
      * Checks interaction with entity and acts based on type.
      * @param initiator Entity that initiates interactions.
      */
-    abstract void checkInteraction(Entity initiator);
+    protected abstract void checkInteraction(Entity initiator);
+
+    /**
+     * Returns the damage mitigation multiplier of the entity.
+     * @return damage mitigation multiplier of the entity.
+     */
+    private double getArmorAbsorption()
+    {
+        if(this.armor >= 100)
+        {
+            return 0.7;
+        }
+        else if(this.armor <= 0)
+        {
+            return 0.0;
+        }
+        else
+        {
+            // Diminishing returns formula.
+            // 0:0  25:.329  50:.509  75:.622  100:.70
+            return ((1.12 * this.armor) / (this.armor + 60));
+        }
+    }
+
+    /**
+     * Damages health of entity based on armor and damage.
+     * @param baseDamage damage taken.
+     */
+    protected void ModifyHealth(int baseDamage)
+    {
+        this.health -= baseDamage * (1 - getArmorAbsorption());
+    }
+
+    /**
+     * Damages health of entity based on armor and damage.
+     * @param baseDamage damage taken.
+     * @param bypassArmor if the damage should ignore armor.
+     */
+    protected void ModifyHealth(int baseDamage, boolean bypassArmor)
+    {
+        if(bypassArmor)
+        {
+            this.health -= baseDamage;
+        }
+        else
+        {
+            ModifyHealth(baseDamage);
+        }
+    }
 }
--- Entity.java
+++ Entity.java
@@ -1,6 +1,8 @@
 import javafx.geometry.Point2D;
 import javafx.scene.image.Image;
 
+import java.awt.*;
+
 /**
  * Represents any entity.
  */
@@ -9,40 +11,171 @@ abstract class Entity
     /**
      * Represents the name of the entity.
      */
-    String name = "";
+    private String name = "";
 
     /**
      * Represents the "attack" stat of the entity. Negative values act as healing.
      */
-    int damage = 0;
+    private int damage = 1;
+
+    /**
+     * Represents multiplier to damage.
+     */
+    private double damageMult = 1.0;
 
     /**
      * Represents the (x,y) coordinates of the entity.
      */
-    Point2D location = null;
+    private Point2D location = null;
 
     /**
      * Represents the (x,y) coordinates of the room the entity is in.
      */
-    Point2D currentRoom = null;
+    private Point2D currentRoom = null;
 
     /**
      * The image displayed.
      */
-    Image graphics = null;
+    private Image graphics = null;
 
     /**
      * Entities cannot damage other entities on the same team, nor buff other entities on other teams
      */
-    String team;
+    private String team = "";
+
+    /**
+     * Returns the name of the entity.
+     * @return name of the entity.
+     */
+    public String getName()
+    {
+        return this.name;
+    }
+
+    /**
+     * Sets the name of the entity.
+     * @param name name of the entity.
+     */
+    protected void setName(String name)
+    {
+        this.name = name;
+    }
+
+    /**
+     * Returns the damage of the entity.
+     * @return damage of the entity.
+     */
+    public int getDamage()
+    {
+        return this.damage;
+    }
+
+    /**
+     * Sets the damage of the entity.
+     * @param damage damage of the entity.
+     */
+    protected void setDamage(int damage)
+    {
+        this.damage = damage;
+    }
+
+    /**
+     * Returns the damage multiplier of the entity.
+     * @return damage multiplier of the entity.
+     */
+    public double getDamageMult()
+    {
+        return this.damageMult;
+    }
+
+    /**
+     * Sets the damage multiplier of the entity.
+     * @param damageMult damage multiplier of the entity.
+     */
+    protected void setDamageMult(double damageMult)
+    {
+        this.damageMult = damageMult;
+    }
+
+    /**
+     * Returns the location of the entity.
+     * @return location of the entity.
+     */
+    public Point2D getLocation()
+    {
+        return this.location;
+    }
+
+    /**
+     * Sets the location of the entity.
+     * @param location location of the entity.
+     */
+    protected void setLocation(Point2D location)
+    {
+        this.location = location;
+    }
+
+    /**
+     * Returns the current room of the entity.
+     * @return current room of the entity.
+     */
+    public Point2D getCurrentRoom()
+    {
+        return this.currentRoom;
+    }
+
+    /**
+     * Sets the current room of the entity.
+     * @param currentRoom current room of the entity.
+     */
+    protected void setCurrentRoom(Point2D currentRoom)
+    {
+        this.currentRoom = currentRoom;
+    }
+
+    /**
+     * Returns the graphical representation of the entity.
+     * @return graphical representation of the entity.
+     */
+    public Image getGraphics()
+    {
+        return this.graphics;
+    }
+
+    /**
+     * Sets the graphical representation of the entity.
+     * @param graphics graphical representation of the entity.
+     */
+    protected void setGraphics(Image graphics)
+    {
+        this.graphics = graphics;
+    }
+
+    /**
+     * Gets the team of the entity.
+     * @return team of the entity.
+     */
+    public String getTeam()
+    {
+        return this.team;
+    }
+
+    /**
+     * Sets the team of the entity.
+     * @param team team of the entity.
+     */
+    protected void setTeam(String team)
+    {
+        this.team = team;
+    }
 
     /**
      * Represents the computation of the entity's movement.
      */
-    abstract void MovementLogic();
+    protected abstract void MovementLogic();
 
     /**
      * Represents the computation of the entity's attacks.
      */
-    abstract void AttackLogic(int direction);
+    protected abstract void AttackLogic(int direction);
 }
