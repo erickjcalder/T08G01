0bb6db0de9fd28d4b49bce1e7c10156c0bf8138c
VladMarianIlie <47425930+VladMarianIlie@users.noreply.github.com>
2019-03-11 10:36:06 -0600

    Add files via upload

---
+++ enemyTwo.java
@@ -0,0 +1,53 @@
+import javafx.geometry.Point2D ;
+
+public class enemyTwo extends Enemy {
+
+  enemyTwo() {
+
+    setHealthMax(100) ;
+    setHealth(100) ;
+    setArmor(1) ;
+    setDamage(2) ;
+    setDamageMult(1) ;
+    setName("Enemy Two") ;
+    setTeam("enemy") ;
+  }
+
+  /**
+  * Attacking
+  *@param direction Direction of attack
+  */
+  @Override
+  protected void AttackLogic(int direction) {
+    //attack
+  }
+  /**
+  * Movement of the enemy.
+  */
+  @Override
+  void MovementLogic() {
+    setLocation(new Point2D(getLocation().getX() + getVelocityX(), getLocation().getY() + getVelocityY())) ;
+  }
+
+  /**
+  * Health based events.
+  */
+  @Override
+  void HealthThresholdEvents() {
+    if(health <= 0) {
+      //enemyTwo Dies
+    }
+  }
+
+  /**
+  * Interactions with other entities.
+  * @param initiator Entity that initiates interactions.
+  */
+  @Override
+  protected void checkInteraction(Entity initiator) {
+    if(initiator instance of player) {
+      ModifyHealth(initiator.getDamage()) ;
+    }
+  }
+
+}
